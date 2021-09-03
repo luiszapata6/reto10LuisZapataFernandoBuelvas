34c1ed14cc09a3150d3e50958fde1b1a99ce5a52
luiszapata6 <80529952+luiszapata6@users.noreply.github.com>
2021-09-03 10:16:40 -0500

    Add files via upload

---
+++ reto10LuisZapataFernandoBuelvas/src/main/java/com/mycompany/reto10luiszapatafernandobuelvas/modelpackage/InterfaceEsfera.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.reto10luiszapatafernandobuelvas.modelpackage;
+
+public interface InterfaceEsfera {
+    
+    public Double volumen();
+    
+    public Double superficie();
+    
+    public Double areaCirculo();
+    
+    public Double perimetroCirculo();
+    
+    // Radio
+    
+    public void setR(Double r);
+    
+    public Double getR();
+    
+}
