0390ec8a8d52fedc1ec21cc17f850debd666d0a3
luiszapata6 <80529952+luiszapata6@users.noreply.github.com>
2021-09-03 10:17:14 -0500

    Delete reto10LuisZapataFernandoBuelvas directory

--- reto10LuisZapataFernandoBuelvas/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.reto10luiszapatafernandobuelvas {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.reto10luiszapatafernandobuelvas to javafx.fxml;
-    exports com.mycompany.reto10luiszapatafernandobuelvas;
-}
